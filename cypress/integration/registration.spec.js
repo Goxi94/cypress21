fa220b00cabf6cee52352285f1109063e340a862
Goxi94 <avasiljevic31@gmail.com>
2022-03-09 12:07:09 +0100

    cypress 21

--- cypress/integration/registration.spec.js
+++ cypress/integration/registration.spec.js
@@ -1,79 +1,74 @@
+/// <reference types="cypress" />
+const Locators = require("../fixtures/Locators.json");
 
-/// <reference types="Cypress" />
+describe('register user', ()=>{
 
-
-describe("registration test", () => {
-
-    beforeEach('visit register page', () => {
+    beforeEach('visit registration page', ()=> {
         cy.visit('/register');
         cy.url().should('contain', '/register');
-    })
+    });
 
-   
-  it("visit register page", () => {
-    cy.visit("/register");
-    cy.url().should("contains", "/register");
-  });
+    it('without first-name', ()=>{ 
+        cy.get(Locators.Register.lastName).type('Pzs');
+        cy.get(Locators.commonElements.emailField).type('markopzs7@test.com');
+        cy.get(Locators.commonElements.passwordField).type('password123');
+        cy.get(Locators.Register.passwordConfirm).type('password123');
+        cy.get(Locators.Register.checkBox).check();
+        cy.get(Locators.commonElements.submitBtn).click();
+        cy.get(Locators.Register.firstName).should('be.empty');
+    });
 
-  it("register without first name", () => {
-    cy.visit("/register");
-    cy.get("#last-name").type("prezime");
-    cy.get("#email").type("test+@mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345!");
-    cy.get(":checkbox").check();
-    cy.get("button").click();
-  });
+    it('without last-name', ()=>{ 
+        cy.get(Locators.Register.firstName).type('Marko');
+        cy.get(Locators.commonElements.emailField).type('markopzs5@test.com');
+        cy.get(Locators.commonElements.passwordField).type('password123');
+        cy.get(Locators.Register.passwordConfirm).type('password123');
+        cy.get(Locators.Register.checkBox).check();
+        cy.get(Locators.commonElements.submitBtn).click();
+        cy.get(Locators.Register.lastName).should('be.empty');
+    });
 
-  it("register without last name", () => {
-    cy.visit("/register");
-    cy.get("#first-name").type("ime");
-    cy.get("#email").type("test+@mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345!");
-    cy.get(":checkbox").check();
-    cy.get("button").click();
-  });
+    it('invalid email', ()=>{    
+        cy.get(Locators.Register.firstName).type('Marko');
+        cy.get(Locators.Register.lastName).type('Pzs');
+        cy.get(Locators.commonElements.emailField).type('markopzs5@testcom');
+        cy.get(Locators.commonElements.passwordField).type('password123');
+        cy.get(Locators.Register.passwordConfirm).type('password123');
+        cy.get(Locators.Register.checkBox).check();
+        cy.get(Locators.commonElements.submitBtn).click();
+        cy.get(Locators.commonElements.alert).should('have.class', 'alert');
+    });
+    
+    it('already used email', ()=>{     
+        cy.get(Locators.Register.firstName).type('Marko');
+        cy.get(Locators.Register.lastName).type('Pzs');
+        cy.get(Locators.commonElements.emailField).type('markopzs3@test.com');
+        cy.get(Locators.commonElements.passwordField).type('password123');
+        cy.get(Locators.Register.passwordConfirm).type('password123');
+        cy.get(Locators.Register.checkBox).check();
+        cy.get(Locators.commonElements.submitBtn).click();
+        cy.get(Locators.commonElements.alert).should('have.class', 'alert'); 
+    });
 
-  it("register with invalid email", () => {
-    cy.visit("/register");
-    cy.get("#first-name").type("ime");
-    cy.get("#last-name").type("prezime");
-    cy.get("#email").type("test+mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345!");
-    cy.get(":checkbox").check();
-    cy.get("button").click();
-  });
+    it('password-confirmation invalid', ()=>{
+        cy.get(Locators.Register.firstName).type('Marko');
+        cy.get(Locators.Register.lastName).type('Pzs');
+        cy.get(Locators.commonElements.emailField).type('markopzs5@test.com');
+        cy.get(Locators.commonElements.passwordField).type('password123');
+        cy.get(Locators.Register.passwordConfirm).type('password123333');
+        cy.get(Locators.Register.checkBox).check();
+        cy.get(Locators.commonElements.submitBtn).click();
+        cy.get(Locators.commonElements.alert).should('have.class', 'alert'); 
+    });
 
-  it("register with invalid password confirmation", () => {
-    cy.visit("/register");
-    cy.get("#first-name").type("ime");
-    cy.get("#last-name").type("prezime");
-    cy.get("#email").type("test+@mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345");
-    cy.get(":checkbox").check();
-    cy.get("button").click();
-  });
+    // it('valid registration', ()=>{           // ------->  valid only once
+    //     cy.get(Locators.Register.firstName).type('Marko');
+    //     cy.get(Locators.Register.lastName).type('Pzs');
+    //     cy.get(Locators.commonElements.emailField).type('markopzs5@test.com');
+    //     cy.get(Locators.commonElements.passwordField).type('password123');
+    //     cy.get(Locators.Register.passwordConfirm).type('password123');
+    //     cy.get(Locators.Register.checkBox).check();
+    //     cy.get(Locators.commonElements.submitBtn).click();
+    // });
 
-  it("register withhout checking terms and conditions", () => {
-    cy.visit("/register");
-    cy.get("#first-name").type("ime");
-    cy.get("#last-name").type("prezime");
-    cy.get("#email").type("test+@mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345!");
-    cy.get("button").click();
-  });
-
-  it("register with valid data", () => {
-    cy.get("#first-name").type("ime");
-    cy.get("#last-name").type("prezime");
-    cy.get("#email").type("test+@mail.com");
-    cy.get("#password").type("Test12345!");
-    cy.get("#password-confirmation").type("Test12345!");
-    cy.get(":checkbox").check();
-    cy.get("button").click();
-  });
 });
